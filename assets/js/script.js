1ed8009ab8a5aba30ca16bd8b8dc8792b6f2dc92
alikamal234-gif <alikamal0675@gmail.com>
2025-11-03 10:30:15 +0100

    finish project avec modifie affichage max itms

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,10 @@
 // Sélection des éléments HTML
-const sizeInput = document.getElementById('sizeInput');
-const sortButton = document.getElementById('sortButton');
-const resetButton = document.getElementById('resetButton');
-const itemList = document.getElementById('itemList');
-const countDisplay = document.getElementById('count');
+const sizeInput = document.getElementById('sizeInput'); // input 
+const sortButton = document.getElementById('sortButton'); // button tri
+const resetButton = document.getElementById('resetButton'); // button Réinitialiser
+const itemList = document.getElementById('itemList'); // list
+const itemLists = document.querySelectorAll('#itemList li');
+const countDisplay = document.getElementById('count');  // span 0 1 2...
 
 // Stocker l'état initial des éléments pour pouvoir les réinitialiser
 const originalItems = Array.from(itemList.children).map(item => item.textContent);
@@ -20,7 +21,7 @@ function updateCount() {
  */
 function filterItemsBySize(maxSize) {
   const items = Array.from(itemList.children);
-
+  
   // TODO: Parcourir les éléments et afficher uniquement ceux
   // dont la longueur du texte est égale ou inférieure à maxSize.
   // Utilisez item.style.display pour gérer l'affichage.
@@ -28,6 +29,21 @@ function filterItemsBySize(maxSize) {
   // Piste pour la boucle : items.forEach(item => { ... });
   
   // Ne pas oublier de mettre à jour le compteur d'éléments visibles
+   
+    
+  for(let i = 0;i<items.length;i++){
+    
+    if(items[i].textContent.length <= maxSize){
+      
+      console.log(items[i].textContent)
+      
+      
+    }else{
+      itemLists[i].style.display="none"
+    }
+  }
+ 
+  
   updateCount();
 }
 
@@ -36,9 +52,12 @@ function filterItemsBySize(maxSize) {
  */
 function sortItemsAlphabetically() {
   const items = Array.from(itemList.children);
-
+  
   // TODO: Filtrer les éléments visibles, les trier par ordre alphabétique
   // avec localeCompare, puis les réinsérer dans itemList dans l'ordre.
+  
+  items.sort((a, b) => a.localeCompare(b));
+  console.log(items);
   
 }
 
